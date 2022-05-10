74b6e0a95ac4c61998d54ca62d170edaf55eb7e3
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-05-10 18:09:21 -0300

    refactor: current level variable

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@ int main(int argc, char** argv){
       prompt(argc, argv);
   }
   else {
-    Difficulty level = load_difficulty(CONFIG_FILE);
+    Difficulty current_level = load_difficulty(CONFIG_FILE);
     
     std::chrono::_V2::system_clock::time_point begin = std::chrono::high_resolution_clock::now();
     
-    bool result = play(level);
+    bool result = play(current_level);
 
     end_game(result, calc_duration(begin));
   }
