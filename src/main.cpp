3d46e904cf090a5230759c2b516bc2bd01e7271b
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-05-09 23:47:09 -0300

    feat: high-level flow on main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
-#include <iostream>
+#include <chrono>
+#include "./utils/time.cpp"
+#include "Game.h"
 
-int main(int argc, char ** argv){
+/**
+ * @brief fluxo principal do programa
+ * 
+ */
+int main(int argc, char** argv){
+  if (argc > 1){
+      prompt(argc, argv);
+  }
+  else {
+    Difficulty level = load_difficulty(CONFIG_FILE);
+    std::chrono::_V2::system_clock::time_point begin = std::chrono::high_resolution_clock::now();
+    bool gameResults = start_game(level);
+    end_game(gameResults, calc_duration(begin));
+  }
   return 0;
 }
