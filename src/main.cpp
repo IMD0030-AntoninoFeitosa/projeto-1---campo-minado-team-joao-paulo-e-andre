a3b7858ef2a2f2b54eabc7aaf46afea6d6d05da4
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-05-09 23:47:40 -0300

    refactor: main function

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,17 @@
 #include "./utils/time.cpp"
 #include "Game.h"
 
-/**
- * @brief fluxo principal do programa
- * 
- */
 int main(int argc, char** argv){
   if (argc > 1){
       prompt(argc, argv);
   }
   else {
     Difficulty level = load_difficulty(CONFIG_FILE);
+    
     std::chrono::_V2::system_clock::time_point begin = std::chrono::high_resolution_clock::now();
+    
     bool gameResults = start_game(level);
+
     end_game(gameResults, calc_duration(begin));
   }
   return 0;
