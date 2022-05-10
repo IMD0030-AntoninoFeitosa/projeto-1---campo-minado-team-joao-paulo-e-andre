698b5d140941a05e4073acb9763d711334c9fd82
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-05-10 18:05:48 -0300

    feat: start game, set difficulty (level) and exit

---
+++ src/Game.cpp
@@ -0,0 +1,115 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <fstream>
+#include <time.h> 
+#include <iomanip>
+#include <sstream>
+
+#include "./utils/strings.cpp"
+#include "Game.h"
+
+void show_usage(){
+  std::cout << "Usage: game [option]" << std::endl;
+  std::cout << "Option:" << std::endl;
+  std::cout << " -h or --help                  Display this information." << std::endl;
+  std::cout << " -d or --difficulty <option>   Change the game difficulty for <option>" << std::endl;
+  std::cout << "                               -b or --beginner" << std::endl;
+  std::cout << "                               -i or --intermediary" << std::endl;
+  std::cout << "                               -a or --advanced" << std::endl;
+}
+
+bool start_game(Difficulty level){
+  std::cout << "Welcome to minesweeper!" << std::endl;
+  return true;
+}
+
+void log_difficulty(const std::string config_file, Difficulty level){
+  std::ofstream file;
+  file.open (config_file.c_str(), std::ifstream::out);
+  if(file.is_open()){
+    switch(level){
+      case Difficulty::beginner:
+        file << 'b';
+        std::cout << "Difficulty set to begginer!" << std::endl;
+        break;
+      case Difficulty::intermediary:
+        file << 'i';
+        std::cout << "Difficulty set to intermediary!" << std::endl;
+        break;
+      case Difficulty::advanced:
+        file << 'a';
+        std::cout << "Difficulty set to advanced!" << std::endl;
+        break;
+    }
+    file.close();
+  }
+}
+
+Difficulty load_difficulty(const std::string config_file){
+  Difficulty level;
+  std::ifstream file;
+  file.open (config_file.c_str(), std::ifstream::in);
+  if(file.is_open()){
+    char c;
+    file >> c;
+    switch(c){
+      case 'b': level = Difficulty::beginner; break;
+      case 'i': level = Difficulty::intermediary; break;
+      case 'a': level = Difficulty::advanced; break;
+    }
+    file.close();
+  } else {
+    log_difficulty(config_file, Difficulty::beginner);
+    level = Difficulty::beginner;
+  }
+  return level;
+}
+
+void prompt(int argc, char** argv) {
+    std::string arg = argv[1];
+    
+    if (arg == "-h" || arg == "-help"){
+      show_usage();
+    }
+    else if (arg == "-d" || arg == "--difficulty"){
+      if (argc > 2){
+        std::string newlevel = argv[2];
+        
+        if(newlevel == "-b" || newlevel == "--beginner"){
+          log_difficulty(CONFIG_FILE, Difficulty::beginner);
+        }
+        else if(newlevel == "-i" || newlevel == "--intermediary"){
+          log_difficulty(CONFIG_FILE, Difficulty::intermediary);
+        }
+        else if(newlevel == "-a" || newlevel == "--advanced"){
+          log_difficulty(CONFIG_FILE, Difficulty::advanced);
+        }
+        else {
+          std::cout << "Unknown difficulty argument: " << newlevel << std::endl;
+          show_usage();
+        }
+      }
+      else {
+        std::cout << "It was expected a difficulty for: " << argv[1] << std::endl;
+        show_usage();
+      }
+    }
+    else {
+      std::cout << "Unknown argument: " << argv[1] << std::endl;
+      show_usage();
+    }
+}
+
+void end_game(bool hasFailed, int seconds){
+  std::string name;
+  
+  if (hasFailed){
+    std::cout << "Game Over!!!" << std::endl;
+    return;
+  }
+  
+  std::cout << "Congrats! You finished the level in " << seconds << " seconds!"<< std::endl;
+}
--- src/Game.h
+++ src/Game.h
@@ -26,61 +26,16 @@ struct Game{
   Point dimensions;
 };
 
-//show usage game options
-void show_usage();
+//reacts to user's inputs such as --difficulty flags etc
+void prompt(int argc, char** argv);
+
+//load current difficulty
+Difficulty load_difficulty(const std::string config_file);
 
 //start the game bases on current level
 bool start_game(Difficulty level);
 
 //end the game and stores player information to the ranking if they win.
 void end_game(bool hasFailed, int seconds);
-
-//store game current level on txt config file
-void store_difficulty(const std::string config_file, Difficulty level);
-
-//load current difficulty
-Difficulty load_difficulty(const std::string config_file);
-
-//show users and their times
-void show_leaderboard();
-
-//reacts to user's inputs such as --difficulty flags etc
-void prompt(int argc, char** argv);
-
-//show map eaccording to the state of each cell
-void show_map(Game game,  Map map);
-
-//stores game settings based on the selected level.
-Game create_game(Difficulty difficulty);
-
-//generates map when called.
-Map create_map(Game game);
-
-//reveal all bombs locations.
-void show_bombs(Game game, Map & map);
-
-//check if the position is valid.
-bool is_valid(Game game, Map & map, int x, int y);
-
-//count mines around cell.
-int count_bombs(Game game, Map & map, int x, int y);
-
-//count flags around cell.
-int count_flags(Game game, Map & map, int x, int y);
-
-//reveals what's around the selected cell.
-bool reveal_around(Game game, Map &map, int x,int y);
-
-//clear cells around 0's.
-void clear_neighbor(Game game, Map & map, int x, int y);
-
-//receives player's input
-bool has_flag(short &x, short &y);
-
-//checks if the player won the game.
-bool check_victory(Game game, Map map);
-
-//game ending in case the user clicks on a bomb
-bool game_lost(Game game, Map map);
 
 #endif
